fe1afba9ecf7563661d53161e1e3801f1c55d97d
Justin Thai <justinthaii@Justins-MBP-2.hsd1.fl.comcast.net>
2021-08-12 02:11:04 -0400

    completed assigning brightness to ASCII and print

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 from PIL import Image
 
-im = Image.open("cat.jpeg") 
+print("Select An Option: ")
+print("1. Example jpg Image")
+print("2. Choose a jpg Image")
+
+im = Image.open("cat.jpeg")
+
+choice = input()
+if (choice == 1):
+    im = Image.open("cat.jpeg") 
+else:
+    print("Type the image name with the extention")
+    imageName = input()
+    im = Image.open(imageName)
+
+
+
+
 width = im.width
 height = im.height
 
-if (width > 300 and height > 300):
-    im = im.resize((300, 300))
-    width = 300
-    height = 300
-elif (width > 300):
-    im = im.resize((300, height))
-    width = 300
-elif (height > 300):
-    im = im.resize((width, 300))
-    height = 300
+if (width > 299 and height > 299):
+    im = im.resize((299, 299))
+    width = 299
+    height = 299
+elif (width > 299):
+    im = im.resize((299, height))
+    width = 299
+elif (height > 299):
+    im = im.resize((width, 299))
+    height = 299
 
 def calcQuotient(index):
     return (index % height)
@@ -29,28 +45,26 @@ def brightnessToASCII(bdata):
     for i in range(width):
         for j in range(height):
             if (bdata[i][j] < 25):
-                bdata[i][j] = "  "
+                bdata[i][j] = "   "
             elif (bdata[i][j] >= 25 and bdata[i][j] < 50):
-                bdata[i][j] = ".."
+                bdata[i][j] = "..."
             elif (bdata[i][j] >= 50 and bdata[i][j] < 75):
-                bdata[i][j] = "::"
+                bdata[i][j] = ":::"
             elif (bdata[i][j] >= 75 and bdata[i][j] < 100):
-                bdata[i][j] = "--"
+                bdata[i][j] = "---"
             elif (bdata[i][j] >= 100 and bdata[i][j] < 125):
-                bdata[i][j] = "=="
+                bdata[i][j] = "==="
             elif (bdata[i][j] >= 125 and bdata[i][j] < 150):
-                bdata[i][j] = "++"
+                bdata[i][j] = "+++"
             elif (bdata[i][j] >= 150 and bdata[i][j] < 175):
-                bdata[i][j] = "**"
+                bdata[i][j] = "***"
             elif (bdata[i][j] >= 175 and bdata[i][j] < 200):
-                bdata[i][j] = "##"
+                bdata[i][j] = "###"
             elif (bdata[i][j] >= 200 and bdata[i][j] < 225):
-                bdata[i][j] = "%%"
+                bdata[i][j] = "%%%"
             elif (bdata[i][j] >= 225 and bdata[i][j] <= 255):
-                bdata[i][j] = "@@"
+                bdata[i][j] = "@@@"
     
-
-# gets data from image into a list
 _data = list(im.getdata())
 
 # turns _data into a 2D list
@@ -67,20 +81,6 @@ for i in range(width):
 brightnessToASCII(bdata)
 
 for i in range(width):
-    #print(*bdata[i], sep='')
-    # for j in range(height):
-    #     print(bdata[i][j])
     print("".join(str(x) for x in bdata[i]))
-    #     #print(bdata, end= "")
-
-# to DO: 
-# assign ascii chars to the brightness value and modify the bdata list from that
-# print bdata without brackets or commas in the terminal.  if image is too big do ctrl - to zoom out, ctrl + to zoom in
-# print(bdata[689]) 69, 63 ascii, 4 for each, some left over
 
 
-# print(data2D[689]) the last one
-
-#print(len(_data))
-
-#im.show()
