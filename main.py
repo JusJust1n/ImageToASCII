2bb61c781ce915ed84c4a725247931fa38e343c4
Justin Thai <justinthaii@Justins-MBP-2.hsd1.fl.comcast.net>
2021-08-13 16:26:18 -0400

    modified some function parameters

--- main.py
+++ main.py
@@ -1,47 +1,15 @@
 from PIL import Image
 
-print("Select An Option: ")
-print("1. Example jpg Image")
-print("2. Choose a jpg Image")
-
-im = Image.open("cat.jpeg")
-
-choice = input()
-if (choice == 1):
-    im = Image.open("cat.jpeg") 
-else:
-    print("Type the image name with the extention")
-    imageName = input()
-    im = Image.open(imageName)
-
-
-
-
-width = im.width
-height = im.height
-
-if (width > 299 and height > 299):
-    im = im.resize((299, 299))
-    width = 299
-    height = 299
-elif (width > 299):
-    im = im.resize((299, height))
-    width = 299
-elif (height > 299):
-    im = im.resize((width, 299))
-    height = 299
-
-def calcQuotient(index):
+def calcQuotient(index, height):
     return (index % height)
 
-def calcRemainder(index):
+def calcRemainder(index, height):
     return (index // height)
 
 def calcBrightness(r, g, b):
     return ((r + g + b) // 3)
 
-def brightnessToASCII(bdata):
-
+def brightnessToASCII(bdata, width, height):
     for i in range(width):
         for j in range(height):
             if (bdata[i][j] < 25):
@@ -65,22 +33,49 @@ def brightnessToASCII(bdata):
             elif (bdata[i][j] >= 225 and bdata[i][j] <= 255):
                 bdata[i][j] = "@@@"
     
-_data = list(im.getdata())
+def main():
+    print("Select An Option: ")
+    print("1. Example jpg Image")
+    print("2. Choose a jpg Image")
 
-# turns _data into a 2D list
-data2D = [[None for _ in range(height)] for _ in range(width)]
-for i in range(len(_data)):
-    data2D[calcRemainder(i)][calcQuotient(i)] = _data[i]
+    im = Image.open("cat.jpeg")
 
-# bdata is brightness data from data2D
-bdata = [[None for _ in range(height)] for _ in range(width)]
-for i in range(width):
-    for j in range(height):
-        bdata[i][j] = calcBrightness(data2D[i][j][0], data2D[i][j][1], data2D[i][j][2])
+    choice = input()
+    if (choice == 1):
+        im = Image.open("cat.jpeg") 
+    elif (choice == 2):
+        print("Type the image name with the extention")
+        imageName = input()
+        im = Image.open(imageName)
 
-brightnessToASCII(bdata)
+    width = im.width
+    height = im.height
 
-for i in range(width):
-    print("".join(str(x) for x in bdata[i]))
+    if (width > 299 and height > 299):
+        im = im.resize((299, 299))
+        width = 299
+        height = 299
+    elif (width > 299):
+        im = im.resize((299, height))
+        width = 299
+    elif (height > 299):
+        im = im.resize((width, 299))
+        height = 299
+        
+    _data = list(im.getdata())
 
+    # turns _data into a 2D list
+    data2D = [[None for _ in range(height)] for _ in range(width)]
+    for i in range(len(_data)):
+        data2D[calcRemainder(i, height)][calcQuotient(i, height)] = _data[i]
+    # bdata is brightness data from data2D
+    bdata = [[None for _ in range(height)] for _ in range(width)]
+    for i in range(width):
+        for j in range(height):
+            bdata[i][j] = calcBrightness(data2D[i][j][0], data2D[i][j][1], data2D[i][j][2])
+    brightnessToASCII(bdata, width, height)
+    for i in range(width):
+        print("".join(str(x) for x in bdata[i]))
 
+if __name__ == "__main__":
+    main()
